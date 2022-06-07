f5d4cf2eeaa8afd679a8b350f5c97d4e5519e0a9
maslov28 <artem.fatefulchoice@mail.ru>
2022-06-07 22:36:26 +0300

    initial

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>{{combineStrings}}</span>
+</template>
+
+<script>
+export default {
+  props: ['date'],
+  computed: {
+    combineStrings() {
+      return this.date + new Date().toLocaleDateString()
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
